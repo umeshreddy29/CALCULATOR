f6226b8e4e7feda8ba0fd2e4c15a0f16ae72a82b
G. UMESH CHANDER REDDY <guthaumeshreddy@gmail.com>
2023-10-12 15:14:46 +0530

    2

--- app/src/main/java/com/example/calculator_app/SplashAcitivity.java
+++ app/src/main/java/com/example/calculator_app/SplashAcitivity.java
@@ -1,24 +1,44 @@
 package com.example.calculator_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class SplashAcitivity extends AppCompatActivity {
+
+    String permissions[] = {"android.permission.WRITE_EXTERNAL_STORAGE","android.permission.CALL_PHONE"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_acitivity);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent iHome = new Intent(SplashAcitivity.this,MainActivity.class);
-                startActivity(iHome);
+        requestPermissions(permissions,80);
+
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 80)
+        {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this,"permissions allowed",Toast.LENGTH_LONG).show();
             }
-        },4000);
+            else
+            {
+                Toast.makeText(this,"permissions denied",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
